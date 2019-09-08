266f7dd01c22d26e7d4ebe10f36751ce44daec59
skypeee <15735184459@163.com>
2019-09-08 23:16:19 +0800

    完善栈与队列

--- 数据结构/code/queue.py
+++ 数据结构/code/queue.py
@@ -5,24 +5,32 @@
 @Date: 2019-09-04 10:13:19
 '''
 class myQueue():
-    def __init__(self):
+    def __init__(self, lenth):
         self._value = []
+        self._lenth = lenth
 
     def entry(self,value):
+        if len(self._value) >= self._lenth:
+            print("队已满")
+            return
         self._value.append(value)
         return self._value
     
     def exit(self):
+        if len(self._value) == 0:
+            print("队空")
+            return
         result = self._value[0]
         del(self._value[0])
         return result
 
 if __name__ == "__main__":
-    a = myQueue()
+    a = myQueue(3)
+    print(a.exit())
+
     print(a.entry(1))
     print(a.entry(2))
     print(a.entry(3))
-    print(a.exit())
     print(a.exit())
     print(a.exit())
 
--- 数据结构/code/stack.py
+++ 数据结构/code/stack.py
@@ -5,24 +5,31 @@
 @Date: 2019-09-04 10:07:11
 '''
 class myStack():
-    def __init__(self):
+    def __init__(self, lenth):
         self._value = []
+        self._lenth = lenth
 
     def pop(self):
+        if len(self._value) == 0:
+            print("栈空")
+            return
         result = self._value[-1]
         del(self._value[-1])
         return result
 
     def push(self, value):
+        if len(self._value) >= self._lenth:
+            print("栈满")
+            return
         self._value.append(value)
         return self._value
 
 if __name__ == "__main__":
-    a = myStack()
+    a = myStack(10)
+    print(a.pop())
     print(a.push(1))
     print(a.push(2))
     print(a.push(3))
-    print(a.pop())
     print(a.push(4))
     print(a.pop())
     print(a.push(1))
